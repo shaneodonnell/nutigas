75c7c7a55772389ab52c43e1f4fb0a729d899e1b
Shane O'Donnell <shaneo@knownormal.com>
2013-07-07 13:44:55 -0400

    Better formatting of Usage & Errors

--- reverseDNS/ptr.php
+++ reverseDNS/ptr.php
@@ -8,8 +8,12 @@
 	validation of the IP address being done.  FIXME 
 */
 
-define(ERROR_BAD_IP,"ERROR: Invalid IP Address");
-define(ERROR_MISSING_IP_PARM,"Error: No ip parameter found.");
+// ERROR MESSAGES
+
+define(ERROR_BAD_IP,"<p style=color:red;font-weight:bold;>ERROR: Invalid IP Address.</p>");
+define(ERROR_MISSING_IP_PARM,"<p style=color:red;font-weight:bold;>Error: No ip parameter found.</p>");
+
+$server = $_SERVER["SERVER_NAME"];
 
 /* This grabs the value of "ip" from the URL parms, tests it minimally and does a reverse DNS lookup */
 
@@ -23,35 +27,34 @@ if (isset($_GET["ip"])) {
 	} else {
 
 		echo ERROR_BAD_IP;
-		usage();
+		usage($server);
 		die();
 	}
 
 } else {
 
 	echo ERROR_MISSING_IP_PARM;
-	usage();
+	usage($server);
 	die();
 }
 
-$host = $_SERVER["HTTP_HOST"];
-$server = $_SERVER["SERVER_NAME"];
-echo "Host = $host<br />Server = $server";
 
-function usage() {
+function usage($server) {
 	echo "<html><head><title>Reverse DNS Lookup Failure & Usage</title></head>";
-	echo "<body><h3>Usage:</h3><br />";
+	echo "<body><h3>Usage:</h3>";
 	echo "This utility does a reverse DNS lookup for the TCP/IP address sent as a value to the <b>ip</b> GET parameter.";
-	echo "<br /><br />";
-	echo "<h4>Example</h4><br />";
 	echo "This utility expects a URI formatted as follows:<br /><br />";
-	echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;http://host.domain.tld/ptr.php?ip=&lt;valid IP address&gt;<br />";
-	echo "<br />so the following URI:<br /><br />";
-	echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;http://host.domain.tld/ptr.php?ip=8.8.8.8<br /><br />";
+	echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;http://$server/ptr.php?ip=&lt;valid IP address&gt;<br />";
+	echo "<br /><br />";
+	echo "<h4>Example</h4>";
+	echo "<br />The following URI:<br /><br />";
+	echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;http://$server/ptr.php?ip=8.8.8.8<br /><br />";
 	echo "will provide the following response:<br /><br />";
 	echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;google-public-dns-a.google.com<br /><br />";
 	echo "If you get back the original IP address (8.8.8.8, in the above example), the lookup failed.<br /><br />";
-	echo "For more information (maybe), go to <https://github.com/shaneodonnell/nutigas/blob/master/README.md>.";
+	echo "For more information (maybe), go to 
+		<a href=https://github.com/shaneodonnell/nutigas/blob/master/README.md>
+		https://github.com/shaneodonnell/nutigas/blob/master/README.md</a>.";
 	echo "</body></html>";
 }
 
